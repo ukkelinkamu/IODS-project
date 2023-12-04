e480a6186f9e0b9bdd3809b5c28170e0f504164d
Aleksi Raudasoja <raudasoja.aleksi@gmail.com>
2023-12-04 14:12:18 +0200

    chapter5

--- create_human.R
+++ create_human.R
@@ -28,3 +28,25 @@ human <- inner_join(hd_c, gii_c, by = "Country")
 
 write.csv(human, file = "human.csv")
 
+# This dataset is from United nations and contains data for human developmental index, life expectancy, education, 
+# and gross national income and couple of other variables per country. Additionally I have created 2 new variables 
+# comparing women and men by education level and labor participation.
+
+# First, lets explore the dataset. it has total of 195 countries and 20 variables
+str(human)
+dim(human)
+
+#excluding unneeded variables
+exclude <- c("Country", "Edu.Ratio", "Labor.Ratio", "Edu.Exp", "Life.Exp", "GNI", "Mat.Mor", "Ado.Birth", "Parli.F")
+
+df <- human[,exclude]
+
+#excluding rows with incomplete data
+df$na <- complete.cases(df)
+df <- df[df$na == TRUE,]
+
+#excluding region rows
+df <- df[1:155,]
+df <- df[,1:9]
+
+write_csv(df, file = "human.csv")
